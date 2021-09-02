263fbb39050dff880786dc7364c343a9c2c200e3
Jason <547490618@qq.com>
2021-09-03 01:25:44 +0800

    add jquery

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,6 +3,13 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
                 test: /\.less$/i,
                 use: [
                     'style-loader',
@@ -14,6 +21,30 @@ module.exports = {
                         },
                     },
                 ],
+            },
+            {
+                test: require.resolve("jquery"),
+                loader: "expose-loader",
+                options: {
+                    exposes: ["$", "jQuery"],
+                },
+            },
+            {
+                test: require.resolve("underscore"),
+                loader: "expose-loader",
+                options: {
+                    exposes: [
+                        "_.map|map",
+                        {
+                            globalName: "_.reduce",
+                            moduleLocalName: "reduce",
+                        },
+                        {
+                            globalName: ["_", "filter"],
+                            moduleLocalName: "filter",
+                        },
+                    ],
+                },
             },
         ],
     },
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,28 @@
-console.log('start');
-document.getElementsByTagName('body')[0].style.backgroundColor = '#eff';
+//暂时直接引入，之后可单文件直接映入
+import $ from "jquery";
+import _, { drop } from "underscore";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../less/common.less"; //全局样式
+
+$(document).on("drop", (e) => {
+  //拖离
+  e.preventDefault();
+});
+$(document).on("dragleave", (e) => {
+  //拖后放
+  e.preventDefault();
+});
+$(document).on("dragenter", (e) => {
+  //拖进
+  e.preventDefault();
+});
+$(document).on("dragover", (e) => {
+  //拖来拖去
+  e.preventDefault();
+});
+
+$("body").on("drop",function (e) {
+  const fileList = e.originalEvent.dataTransfer.files; //获取文件对象
+  console.log(fileList);
+});
+$('.box').on();
